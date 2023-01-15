cd00b76ad48e6e200c9509f6f9a2424fc64af3ae
omen <kybar089@gmail.com>
2023-01-15 17:57:02 -0600

    updated Thought.js model

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,36 @@
 const mongoose = require('mongoose')
 const moment = require('moment')
 
+const ReactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+},  {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false,
+    }
+)
+
+
+
 const ThoughtSchema = new mongoose.Schema({
     thoughtText: {
         type: String,
